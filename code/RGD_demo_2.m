dbaa385c14d88ec1f3d102e08362d07e83abfbc1
Jiyuan <liu_jiyuan@outlook.com>
2020-06-14 20:21:00 +0800

    find the drawbacks of the optimization 1; add optimization 2; complete the code;

---
+++ code/RGD_demo_2.m
@@ -0,0 +1,47 @@
+function [ nbeta, sbeta, obj] = RGD_demo_2( M, maxiter)
+%RGD_DEMO 
+%   To solve min_\beta \beta^t M \beta s.t. \sum \beta = 1, \beta >= 0 via
+%   reduced gradient descent algorithm
+
+alpha = 1e-8;
+
+m = size(M, 1);
+beta = ones(m,1)/m;
+sbeta = cell(1);
+t = 1;
+sbeta{t} = beta;
+obj(t) = beta'*M*beta;
+
+flag = 1;
+while flag==1
+%     u = find(beta==max(beta));
+%     u = u(1);
+    u=1;
+    delta = 2*M*beta;
+    
+    for p=1:m
+       if beta(p)<=eps && delta(p)>0
+           delta(p) = 0;
+       end
+    end
+    
+    d = -(delta - delta(u));
+    d(u) = -(m*delta(u) - sum(delta));
+    
+    nbeta = beta + alpha*d;
+
+    if max(nbeta - beta) < 1e-6 || t>maxiter
+        flag = 0;
+    end
+
+    t = t+1;
+    beta = nbeta;
+    sbeta{t} = nbeta;
+    obj(t) = beta'*M*beta;
+    
+
+    
+end 
+
+end
+
---
+++ code/main.m
@@ -0,0 +1,73 @@
+data_name = 'proteinFold';
+data_path = 'D:\Work\datasets\Kmatrix\';
+load([data_path, data_name, '_Kmatrix.mat'], 'KH');
+
+numker = size(KH, 3);
+K = kcenter(KH);
+K = knorm(K);
+
+M = zeros(numker);
+for p=1:numker
+    for q=1:numker
+        M(p,q) = trace(K(:,:,p)*K(:,:,q));
+    end
+end
+M = (M+M')/2;
+
+% ----------------------------------------------------------
+% QP problem: quadprog()
+H = M;
+f = [];
+A = [];
+b = [];
+Aeq = ones(1,numker);
+beq = 1;
+lb = zeros(numker,1);
+ub = ones(numker,1);
+[beta_1,fval,exitflag] = quadprog(H,f,A,b,Aeq,beq,lb,ub);
+
+
+% ----------------------------------------------------------
+% QP problem: RGD_1
+maxiter = 1e5;
+[beta_2, sbeta_2, obj_2] = RGD_demo_1(M, maxiter);
+
+% ----------------------------------------------------------
+% QP problem: RGD_1
+maxiter = 1e5;
+[beta_3, sbeta_3, obj_3] = RGD_demo_2(M, maxiter);
+
+figure(1)
+set(gcf,'position',[0,0,1000,400]);
+bar([beta_1,beta_2,beta_3]);
+ylim([0, 0.5]);
+% legend('quadprog()', 'RGD 1', 'RGD 2');
+legend('\fontsize{12}{quadprog()}', '\fontsize{12}{RGD 1}', '\fontsize{12}{RGD 2}');
+title('\fontsize{16}{Protein Fold}');
+xlabel('\fontsize{16}{Index}');
+ylabel('\fontsize{16}{\beta}');
+axis on;
+grid on;
+
+
+figure(2)
+set(gcf,'position',[0,0,1000,500]);
+sp1 = subplot(1,2,1);
+plot(obj_2,'LineWidth',2);
+hold on;
+title('\fontsize{16}{RGD 1 on Protein Fold}');
+xlabel('\fontsize{16}{Index}');
+ylabel('\fontsize{16}{Objective Value}');
+axis square;
+grid on;
+sp2 = subplot(1,2,2);
+plot(obj_3,'LineWidth',2);
+hold on;
+title('\fontsize{16}{RGD 2 on Protein Fold}');
+xlabel('\fontsize{16}{Index}');
+ylabel('\fontsize{16}{Objective Value}');
+axis square;
+grid on;
+
+
+
