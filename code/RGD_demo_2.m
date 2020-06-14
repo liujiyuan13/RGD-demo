854d84518dd29f6a98f01818f9491467a5b4df3b
Jiyuan <liu_jiyuan@outlook.com>
2020-06-14 22:46:11 +0800

    revise the optimization two in latex and code

--- code/RGD_demo_2.m
+++ code/RGD_demo_2.m
@@ -14,23 +14,36 @@ obj(t) = beta'*M*beta;
 
 flag = 1;
 while flag==1
-%     u = find(beta==max(beta));
-%     u = u(1);
-    u=1;
+    
     delta = 2*M*beta;
     
+    fix_indx = zeros(m,1);
     for p=1:m
+       % for convex function: delta(p)>0
        if beta(p)<=eps && delta(p)>0
-           delta(p) = 0;
+           fix_indx(p) = 1;
        end
     end
+    fix_indx = boolean(fix_indx);
+    fix_sum = sum(beta(fix_indx));
+    beta(fix_indx) = 0;
+    delta(fix_indx) = 0;
     
-    d = -(delta - delta(u));
-    d(u) = -(m*delta(u) - sum(delta));
+    var_indx = ~fix_indx;
+    % beta values on fix_indx sometimes are negative, for relatively bigger
+    % step size. spare them to the var_indx.
+    beta(var_indx) = beta(var_indx) + fix_sum/sum(var_indx);
+
+    u = find(beta==max(beta));
+    u = u(1);
+    
+    d = zeros(m,1);
+    d(var_indx) = -(delta(var_indx) - delta(u));
+    d(u) = -(sum(var_indx)*delta(u) - sum(delta(var_indx)));
     
     nbeta = beta + alpha*d;
+    if max(abs(nbeta - beta)) < 1e-6 || t>maxiter
 
-    if max(nbeta - beta) < 1e-6 || t>maxiter
         flag = 0;
     end
 
